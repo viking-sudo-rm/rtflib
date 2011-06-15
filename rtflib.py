f895071fbe09b88f59309715ab9b2141808117ce
viking-sudo-rm <vikingarnir.will@gmail.com>
2011-06-15 07:48:54 -0700

    My git is being annoying, so I posted the newest version through a browser

--- rtflib.py
+++ rtflib.py
@@ -1,7 +1,7 @@
 """
 
-rtflib v. 1.1 Beta
-WM, 2011
+rtflib v. 1.2 Beta
+author: lambdaviking
 
 Purpose:
 Allows you to write RTF files easily
@@ -13,9 +13,27 @@ from rtflib import *
 file = RTF("helloworld.rtf")
 file.startfile()
 file.addstrict()
-line = Line("hello world", color=Color(255,0,0))
-file.addelement(line)
+file.addtext("hello world", color=Color(255,0,0))
 file.writeout()
+
+Methods to add to the RTF:
+RTF.addstrict() -- adds strict headers to the top of the file
+RTF.addelement(element) -- adds an element such as text or a table to the file
+RTF.addtext(text,color=None,format=None) -- a shorthand for adding Line elements to the file with RTF.addelement
+
+Elements:
+Elements are objects that can be added to RTFs using this module.
+
+Table -- a table
+Row -- a row in a table
+Line -- a line of text that can be packed into the file or a Row
+HyperString -- a line of custom raw RTF markup to be placed into the file
+
+Descriptor Classes:
+Descriptor classes can be passed to the constructor of elements as optional arguments.
+
+Color(r=0,g=0,b=0) -- describes the font color of an element
+Format(bold=False,underline=False,intalicized=False,size=24) -- describes the text formatting of an element
 
 
 """
@@ -39,10 +57,11 @@ class Color:
 #format (bold, underline, etc.) object for input
 class Format:
 	"""object for format (bold, underline, etc.) of text"""
-	def __init__(self,bold=False,underline=False,italicized=False):
+	def __init__(self,bold=False,underline=False,italicized=False,size=24):
 		self.bold = bold
 		self.underline = underline
 		self.italicized = italicized
+		self.size = size
 
 #object for entering a table to the RTF
 class Table:
@@ -142,10 +161,12 @@ class RTF:
 			if element.format and element.format.underline: wf.write("\\ul ")
 			if element.format and element.format.bold: wf.write("\\b ")
 			if element.format and element.format.italicized: wf.write("\\i ")
+			if element.format: wf.write("\\fs" + str(element.format.size) + " ")
 			wf.write(render(element) + "\n")
 			if element.format and element.format.underline: wf.write("\\ulnone ")
 			if element.format in dir(element) and element.format.bold: wf.write("\\b0 ")
 			if element.format and element.format.italicized: wf.write("\\i0 ")
+			if element.format: wf.write("\\fs24 ")
 			if "__cid__" in dir(element): wf.write("\\cf0 \ ")
 		wf.write("}")
 		wf.close()
