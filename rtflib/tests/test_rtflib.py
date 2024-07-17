d590433ef77ce3ee75eb4f4b5b8734b3a4e39bba
Neon22 <mschafer@wireframe.biz>
2024-07-17 13:28:37 +1200

    added multi-line test
    with color

--- rtflib/tests/test_rtflib.py
+++ rtflib/tests/test_rtflib.py
@@ -19,6 +19,26 @@ HELLO_WORLD_RED = """
 }
 """
 
+HELLO_WORLD_MULTI = """
+{\rtf1\ansi\deff0
+{
+\colortbl;
+\red255\green0\blue0;
+\red0\green255\blue0;
+}
+hello world. A Line of text.\
+\ 
+\cf1 hello world. A Red line\
+\ \cf0 \ 
+hello world. A mix of 
+\cf2 Green \cf0 \ 
+and 
+\cf1 Red\cf0 \ 
+ on a single line.\
+\ 
+}
+"""
+
 TABLE = """
 {\\rtf1\\ansi\\deff0
 here is a table:
@@ -49,6 +69,18 @@ class TestRtflib(unittest.TestCase):
         rtf = Rtf()
         rtf.add(Line("hello world", color=Color(255, 0, 0)))
         with open(os.path.join(self.path, "helloworld-red.rtf")) as fh:
+            self.assertEqual(rtf.rtf_code.strip(), fh.read())
+    
+    def test_hello_world_multi(self):
+        rtf = Rtf()
+        rtf.add(Line("hello world. A Line of text.\n"))
+        rtf.add(Line("hello world. A Red line\n", color=Color(255, 0, 0)))
+        rtf.add(Line("hello world. A mix of "))
+        rtf.add(Line("Green ", color=Color(0, 255, 0)))
+        rtf.add(Line("and "))
+        rtf.add(Line("Red", color=Color(255, 0, 0)))
+        rtf.add(Line(" on a single line.\n"))
+        with open(os.path.join(self.path, "helloworld-multi.rtf")) as fh:
             self.assertEqual(rtf.rtf_code.strip(), fh.read())
     
     def test_table(self):
